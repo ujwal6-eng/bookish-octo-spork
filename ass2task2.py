fc8d1b6c8dc8a1b502c29612b56b5e117d398d90
ujwal6-eng <anandujwal6@gmail.com>
2025-08-26 10:13:28 +0530

    Add files via upload

---
+++ ass2task2.py
@@ -0,0 +1,12 @@
+# Assignment 2 - Module 3
+# Task 2: Sum of Integers from 1 to 50 using a loop.
+
+# steps 1: Initialize sum variable
+total = 0
+
+# Steps 2: Use a for to iterate from 1 to 50
+for num in range(1, 11):
+    total += num  # Add each number to total
+
+#Step 3: Display the final sum
+print(f"The sum of number from 1 to 10 is {total}.")
